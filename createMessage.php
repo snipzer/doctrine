e3b4028e9a81516280da4c9ee45748af3b3f1a18
snipzer <l.arif@laposte.net>
2017-04-04 14:15:18 +0200

    Fichier permettant la céation d'un nouveau message

--- createMessage.php
+++ createMessage.php
@@ -1,8 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: snipzer
- * Date: 04/04/17
- * Time: 14:09
- */
-?>
+// createMessage.php
+require_once "bootstrap.php";
+
+use Imie\Entity\Message;
+
+$newMessage = $argv[1];
+
+try
+{
+    $message = new Message();
+    $message->setName($newMessage);
+
+    $entityManager->persist($message);
+    $entityManager->flush();
+
+    echo "Created Product with ID " . $message->getId() . "\n";
+}
+catch(Exception $e)
+{
+    $e->getMessage();
+}
