e33356e07f2bd56c4e5a331a227ffa3b201f7f29
snipzer <l.arif@laposte.net>
2017-04-04 15:49:17 +0200

    Mise en forme du var_dump

--- testRepository.php
+++ testRepository.php
@@ -8,7 +8,9 @@ try
     $ProductRepository = $entityManager->getRepository("Imie\Entity\Product");
     $toto = $ProductRepository->getProductsOrderedByName();
 
+    echo "<pre>";
     var_dump($toto);
+    echo "</pre>";
 }
 catch(Exception $e)
 {
