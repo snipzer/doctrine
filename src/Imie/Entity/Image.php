171a3efa91766d2b7adcefed5b95198bd4d8a95b
snipzer <l.arif@laposte.net>
2017-04-05 12:46:15 +0200

    Création de classe pour relation monodirectionnel

---
+++ src/Imie/Entity/Image.php
@@ -0,0 +1,79 @@
+<?php
+namespace Imie\Entity;
+/**
+ * @Table(name="images")
+ * @Entity
+ */
+class Image
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @Column(type="string", length=150)
+     */
+
+    protected $name;
+    /**
+     * @Column(type="string", length=10)
+     */
+
+    protected $width;
+
+    /**
+     * @Column(type="string", length=10)
+     */
+    protected $height;
+
+    public function __construct($width = "150px", $height = "150px")
+    {
+        $this->setHeight($height);
+        $this->setWidth($width);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+
+}
+
+?>
---
+++ src/Imie/Entity/ProductImage.php
@@ -0,0 +1,60 @@
+<?php
+namespace Imie\Entity;
+
+/**
+ * @Table(name="productImage")
+ * @Entity
+ */
+
+class ProductImage
+{
+    /**
+     * @id @Column(type="integer")
+     * @GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @Column(type="string", length=140)
+     */
+    protected $name;
+
+    /**
+     * @OneToOne(targetEntity="Imie\Entity\Image")
+     * @JoinColumn(nullable = false)
+     */
+    private $image;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage(Image $image = null)
+    {
+        $this->image = $image;
+    }
+
+
+}
+?>
